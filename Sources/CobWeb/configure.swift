2cab0397923311ed2cbe7a44458de2325e73330d
Christopher Wainwright <chris.wainwright@atomicmedia.co.uk>
2025-08-16 20:28:33 +0100

    Remove TODO Example References

--- Sources/CobWeb/configure.swift
+++ Sources/CobWeb/configure.swift
@@ -10,7 +10,6 @@ public func configure(_ app: Application) async throws {
 
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
 
-    app.migrations.add(CreateTodo())
 
     // register routes
     try routes(app)
